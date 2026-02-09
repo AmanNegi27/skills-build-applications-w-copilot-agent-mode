67b80816f7ccb8df665df4ff4a62d09d80de8edd
Aman Negi <115018060+AmanNegi27@users.noreply.github.com>
2026-02-09 07:06:59 +0000

    Fix urls.py for codespace API root

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,27 +1,30 @@
-"""octofit_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import os
 
-CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
-api_prefix = 'api/'
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    codespace = os.getenv("CODESPACE_NAME")
+
+    if codespace:
+        base_url = f"https://{codespace}-8000.app.github.dev"
+    else:
+        base_url = "http://localhost:8000"
+
+    return Response({
+        "users": f"{base_url}/api/users/",
+        "teams": f"{base_url}/api/teams/",
+        "activities": f"{base_url}/api/activities/",
+        "workouts": f"{base_url}/api/workouts/",
+        "leaderboard": f"{base_url}/api/leaderboard/",
+    })
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(f'{api_prefix}', include('octofit_tracker.api_urls')),
+    path('api/', include('octofit_tracker.api_urls')),
+    path('', api_root, name='api-root'),
 ]
